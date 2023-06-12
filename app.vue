137c9731c19d79d0a2bf08a071eed3b073bdc5f5
Lo King wei <kingwei.lo@live.com.my>
2023-06-13 00:42:12 +0800

    added carousel and query param

--- app.vue
+++ app.vue
@@ -1,13 +1,33 @@
 <script setup>
-const res = await useFetch('https://succinct-hamburger-1b1.notion.site/api/v3/loadCachedPageChunk', { method: 'POST', body: {"page":{"id":"55985dd4-cdeb-4039-bd75-475ba457b123"},"limit":30,"cursor":{"stack":[]},"chunkNumber":0,"verticalColumns":false}});
-console.log(Object.values(res.data.value.recordMap.block));
-var images = Object.values(res.data.value.recordMap.block).filter(b => b.value.type === 'image').map(b => ({url: b.value.properties.source[0][0], id: b.value.id}))
-.map(s => `https://succinct-hamburger-1b1.notion.site/image/${encodeURIComponent(s.url)}?id=${s.id}&table=block&spaceId=12c6b7f7-1d70-4eea-8ae0-671efc6e35d5&width=1610&userId=&cache=v2`)
-console.log(images);
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
+const route = useRoute()
+const id = route.query.id;
+const domain = route.query.domain;
+let images = ref([]);
+if(id != null && domain != null) {
+  const res = await useFetch(`https://${domain}.notion.site/api/v3/loadCachedPageChunk`, { method: 'POST', body: {"page":{"id":id},"limit":30,"cursor":{"stack":[]},"chunkNumber":0,"verticalColumns":false}});
+  images.value = Object.values(res.data.value.recordMap.block).filter(b => b.value.type === 'image').map(b => ({url: b.value.properties.source[0][0], id: b.value.id}))
+  .map(s => `https://${domain}.notion.site/image/${encodeURIComponent(s.url)}?id=${s.id}&table=block&spaceId=12c6b7f7-1d70-4eea-8ae0-671efc6e35d5&width=1610&userId=&cache=v2`)
+}
 </script>
 
+<style>
+.responsive {
+  width: 100%;
+  max-height: 400px;
+  height: auto;
+}
+</style>
+
 <template>
-  <div>
-    <img v-for="img in images" :key="img" :src="img" />
-  </div>
+  <carousel :items-to-show="1.5">
+    <slide v-for="img in images" :key="img">
+      <img :src="img" class="responsive"/>
+    </slide>
+    <template #addons>
+      <navigation />
+      <pagination />
+    </template>
+  </carousel>
 </template>
